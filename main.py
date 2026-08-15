ebb73684718811ca23e849bdcb04a073d3266fdd
freelance1911-design <freelancers1911@gmail.com>
2026-08-15 16:27:25 +0530

    another change

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173" , "https://chatbot-seven-olive-68.vercel.app/"],
+    allow_origins=["http://localhost:5173" , "https://chatbot-seven-olive-68.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
